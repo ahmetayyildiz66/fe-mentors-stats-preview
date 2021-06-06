d11fe0684804934a1911bb2c1aca381f0cd0bcfa
ahmetayyildiz66 <ahmetayyildiz66@gmail.com>
2021-06-06 08:17:00 +0300

    Style guide styles were applied

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'very-dark-blue': 'hsl(233, 47%, 7%)',
+        'dark-desaturated-blue': 'hsl(244, 38%, 16%)',
+        'soft-violet': 'hsl(277, 64%, 61%)',
+        'light-white': 'hsla(0, 0%, 100%, 0.75)',
+        'very-light-white': 'hsla(0, 0%, 100%, 0.6)'
+      },
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif'],
+        'lexend-deca': ['Lexend Deca', 'sans-serif'],
+      }
+    },
+    screens: {
+      'sm': '375px',
+      'lg': '1440px'
+    }
   },
   variants: {
     extend: {},
